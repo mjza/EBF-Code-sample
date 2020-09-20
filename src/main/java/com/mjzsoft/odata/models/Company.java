9457aceca77e9808454cc157507f1c6096aff5a7
mjbza <mahdijaberzadehansari@yahoo.co.uk>
2020-09-20 21:02:59 +0200

    Average added

--- src/main/java/com/mjzsoft/odata/models/Company.java
+++ src/main/java/com/mjzsoft/odata/models/Company.java
@@ -2,10 +2,14 @@ package com.mjzsoft.odata.models;
 
 import javax.persistence.*;
 
+import com.mjzsoft.odata.annotations.SAPLineItem;
 import com.mjzsoft.odata.annotations.Sap;
 
 import java.io.Serializable;
+import java.util.Iterator;
 import java.util.Set;
+import org.apache.commons.math3.util.Precision;
+import org.hibernate.annotations.ColumnDefault;
 
 /**
  * The persistent class for the companies database table.
@@ -22,17 +26,25 @@ public class Company implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@Sap(filterable=true, sortable=true)
+	@Sap(filterable = true, sortable = true, creatable = true, updatable = false)
+	@SAPLineItem
 	@Column(name = "id", unique = true, nullable = false, length = 255)
 	private String id;
-	
-	@Sap(filterable=true, sortable=true, creatable=true, updatable=true)
+
+	@Sap(filterable = true, sortable = true, creatable = true, updatable = true)
+	@SAPLineItem
 	@Column(name = "name", nullable = false, length = 255)
 	private String name;
 
-	// bi-directional many-to-one association to Employee	
+	// bi-directional many-to-one association to Employee
 	@OneToMany(mappedBy = "company")
 	private Set<Employee> employees;
+
+	@Sap(filterable = true, sortable = true, creatable = false, updatable = false, deletable = false)
+	@SAPLineItem
+	@Column(name = "average_salary", nullable = false,  updatable=false, insertable=false)
+	@ColumnDefault("0.0")
+	private double averageSalary;
 
 	public Company() {
 	}
@@ -73,6 +85,24 @@ public class Company implements Serializable {
 		employee.setCompany(null);
 
 		return employee;
+	}
+
+	public double getAverageSalary() {
+		int size = getEmployees() != null ? getEmployees().size() : 0;
+		if (size > 0) {
+			double sumSalary = 0;
+			Iterator<Employee> iterator = getEmployees().iterator();
+			while (iterator.hasNext()) {
+				Employee employee = iterator.next();
+				sumSalary += employee.getSalary();
+			}
+			return Precision.round((sumSalary / size), 2);
+		}
+		return this.averageSalary;
+	}
+
+	public void setAverageSalary(double averageSalary) {
+		this.averageSalary = 0.0;
 	}
 
 }
