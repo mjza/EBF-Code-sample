8f8b7f459acdb511ce1bc409040bd4061e9c9e3a
mjbza <mahdijaberzadehansari@yahoo.co.uk>
2020-09-21 10:25:06 +0200

    Copyright text added

--- src/main/java/com/mjzsoft/odata/models/Employee.java
+++ src/main/java/com/mjzsoft/odata/models/Employee.java
@@ -1,3 +1,17 @@
+/*
+ * Licensed under the MIT License
+ *
+ * Copyright (c) 2020 Mahdi Jaberzadeh Ansari
+ * 
+ * you may not use this file except in compliance with the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.mjzsoft.odata.models;
 
 import java.io.Serializable;
