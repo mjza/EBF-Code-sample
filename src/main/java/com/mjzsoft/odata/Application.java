79e56674c2f0ebd34081ba5b6ac52335077562f6
mjbza <mahdijaberzadehansari@yahoo.co.uk>
2020-09-21 07:50:33 +0200

    fixed jar static file serving

--- src/main/java/com/mjzsoft/odata/Application.java
+++ src/main/java/com/mjzsoft/odata/Application.java
@@ -1,15 +1,11 @@
 package com.mjzsoft.odata;
 
-import java.util.TimeZone;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.StandardEnvironment;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
@@ -23,12 +19,7 @@ public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		logger.info("Application has been started.");		
-		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-		ConfigurableEnvironment environment = new StandardEnvironment();
-		environment.setActiveProfiles("dev");
-		SpringApplication springApplication = new SpringApplication(Application.class);
-		springApplication.setEnvironment(environment);
-		springApplication.run(args);
+		SpringApplication.run(Application.class);
 		
 	}
 
